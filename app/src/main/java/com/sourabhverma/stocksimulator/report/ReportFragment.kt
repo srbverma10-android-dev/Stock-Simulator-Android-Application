9ad3ef041055e7a3f1f58d93a0a4e8fdc8b9a0d1
SOURABH KUMAR VERMA <1906286@kiit.ac.in>
2021-10-25 19:06:42 +0530

    ui handle for report

--- app/src/main/java/com/sourabhverma/stocksimulator/report/ReportFragment.kt
+++ app/src/main/java/com/sourabhverma/stocksimulator/report/ReportFragment.kt
@@ -3,15 +3,18 @@ package com.sourabhverma.stocksimulator.report
 import android.app.Activity.RESULT_OK
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.drawable.AnimationDrawable
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.widget.doOnTextChanged
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.snackbar.Snackbar
+import com.sourabhverma.stocksimulator.R
 import com.sourabhverma.stocksimulator.base.BaseFragment
 import com.sourabhverma.stocksimulator.databinding.FragmentReportBinding
 import com.sourabhverma.stocksimulator.utils.CacheHelperClass
@@ -19,7 +22,7 @@ import com.sourabhverma.stocksimulator.utils.CommonUtils
 
 
 class ReportFragment : BaseFragment<FragmentReportBinding, ReportViewModel>(), ClickListener {
-    override fun getLayoutId(): Int = com.sourabhverma.stocksimulator.R.layout.fragment_report
+    override fun getLayoutId(): Int = R.layout.fragment_report
 
     override fun getViewModel(): Class<ReportViewModel> = ReportViewModel::class.java
 
@@ -29,6 +32,8 @@ class ReportFragment : BaseFragment<FragmentReportBinding, ReportViewModel>(), C
     private val screenShotAdapter = ScreenShotAdapter()
 
     private val build = "{\"buildName\":\"Google pixal 4a\"}"
+
+    private var animationDrawable = AnimationDrawable()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -60,7 +65,7 @@ class ReportFragment : BaseFragment<FragmentReportBinding, ReportViewModel>(), C
             if (!text.isNullOrEmpty() && text.isNotBlank()) {
                 if (!CommonUtils().isValidEmail(text)){
                     binding.emailEditTextLayout.isErrorEnabled = true
-                    binding.emailEditTextLayout.error = getString(com.sourabhverma.stocksimulator.R.string.enter_email_error)
+                    binding.emailEditTextLayout.error = getString(R.string.enter_email_error)
                 } else {
                     binding.emailEditTextLayout.isErrorEnabled = false
                     binding.emailEditTextLayout.error = null
@@ -74,59 +79,65 @@ class ReportFragment : BaseFragment<FragmentReportBinding, ReportViewModel>(), C
         binding.feedbackEditText.doOnTextChanged { text, _, _, _ ->
             if (text != null && text.isEmpty() && text.isBlank()) {
                 binding.feedbackEditTextLayout.isErrorEnabled = true
-                binding.feedbackEditTextLayout.error = getString(com.sourabhverma.stocksimulator.R.string.this_can_not_be_null)
+                binding.feedbackEditTextLayout.error = getString(R.string.this_can_not_be_null)
             } else {
                 binding.feedbackEditTextLayout.isErrorEnabled = false
                 binding.feedbackEditTextLayout.error = null
             }
         }
 
+        binding.submitButton.isClickable = true
         binding.submitButton.setOnClickListener {
             if (!binding.emailEditTextLayout.isErrorEnabled && !binding.feedbackEditTextLayout.isErrorEnabled &&
                     CommonUtils().isValidEmail(binding.emailEditText.text.toString()) && binding.feedbackEditText.text != null &&
                     binding.feedbackEditText.text.toString().isNotBlank() && binding.feedbackEditText.text.toString().isNotEmpty()){
-                //upload data to the server
-                if (CommonUtils().isInternetWorking(requireContext())) {
-                    arguments?.getString(CommonUtils().type)?.let { it1 ->
-                        context?.let { it2 -> CacheHelperClass.getLogFile(it2) }?.let { it3 ->
-                            viewModel.addReport(binding.emailEditText.text.toString(),
-                                it1, binding.feedbackEditText.text.toString(), bitmapArray,
-                                it3, build, requireContext()
-                            )
-                        }
-                    }
-                } else {
-                    view?.let { it1 ->
-                        val snackbar: Snackbar =
-                            Snackbar.make(it1, com.sourabhverma.stocksimulator.R.string.noInternetConnection, Snackbar.LENGTH_SHORT)
-                        val snackbarLayout = snackbar.view
-                        val textView =
-                            snackbarLayout.findViewById<View>(com.sourabhverma.stocksimulator.R.id.snackbar_text) as TextView
-                        textView.setCompoundDrawablesWithIntrinsicBounds(
-                            0,
-                            0,
-                            com.sourabhverma.stocksimulator.R.drawable.ic_baseline_error_outline_24,
-                            0,
-                        )
-                        textView.compoundDrawablePadding =
-                            resources.getDimensionPixelOffset(com.sourabhverma.stocksimulator.R.dimen.snackbar_icon_padding)
-                        snackbar.show()
+                        binding.submitButton.text = null
+                        val loadingDrawable = AppCompatResources.getDrawable(requireContext(), R.drawable.animated_loading_drawable)
+                        binding.submitButton.icon = loadingDrawable
+                        animationDrawable =  loadingDrawable as AnimationDrawable
+                        animationDrawable.start()
+                        if (CommonUtils().isInternetWorking(requireContext())) {
+                            arguments?.getString(CommonUtils().type)?.let { it1 ->
+                                context?.let { it2 -> CacheHelperClass.getLogFile(it2) }?.let { it3 ->
+                                    it.isClickable = false
+                                    viewModel.addReport(binding.emailEditText.text.toString(),
+                                        it1, binding.feedbackEditText.text.toString(), bitmapArray,
+                                        it3, build, requireContext()
+                                    )
+                                }
+                            }
+                        } else {
+                            view?.let { it1 ->
+                                val snackbar: Snackbar =
+                                    Snackbar.make(it1, R.string.noInternetConnection, Snackbar.LENGTH_SHORT)
+                                val snackbarLayout = snackbar.view
+                                val textView =
+                                    snackbarLayout.findViewById<View>(R.id.snackbar_text) as TextView
+                                textView.setCompoundDrawablesWithIntrinsicBounds(
+                                    0,
+                                    0,
+                                    R.drawable.ic_baseline_error_outline_24,
+                                    0,
+                                )
+                                textView.compoundDrawablePadding =
+                                    resources.getDimensionPixelOffset(R.dimen.snackbar_icon_padding)
+                                snackbar.show()
 
-                    }
-                }
+                            }
+                        }
 
             } else {
                 writeLog(CommonUtils().showError, "IN:- REPORT-FRAGMENT")
                 if (!CommonUtils().isValidEmail(binding.emailEditText.text.toString())){
                     binding.emailEditTextLayout.isErrorEnabled = true
-                    binding.emailEditTextLayout.error = getString(com.sourabhverma.stocksimulator.R.string.enter_email_error)
+                    binding.emailEditTextLayout.error = getString(R.string.enter_email_error)
                 } else {
                     binding.emailEditTextLayout.isErrorEnabled = false
                     binding.emailEditTextLayout.error = null
                 }
                 if (binding.feedbackEditText.text.toString().isEmpty() && binding.feedbackEditText.text.toString().isBlank()){
                     binding.feedbackEditTextLayout.isErrorEnabled = true
-                    binding.feedbackEditTextLayout.error = getString(com.sourabhverma.stocksimulator.R.string.this_can_not_be_null)
+                    binding.feedbackEditTextLayout.error = getString(R.string.this_can_not_be_null)
                 } else {
                     binding.feedbackEditTextLayout.isErrorEnabled = false
                     binding.feedbackEditTextLayout.error = null
@@ -139,18 +150,20 @@ class ReportFragment : BaseFragment<FragmentReportBinding, ReportViewModel>(), C
         }
 
         viewModel.getAddReportLiveData().observe(viewLifecycleOwner, {
+            binding.submitButton.isClickable = true
             if (it?.code != null && it.code == 201){
                 Toast.makeText(context, "success", Toast.LENGTH_SHORT).show()
             } else {
                 Toast.makeText(context, "fail", Toast.LENGTH_SHORT).show()
             }
+            binding.cancelButton.performClick()
         })
 
     }
 
     private fun setTextAccToOptionSelectedByUser() {
         writeLog(CommonUtils().passedData, "${CommonUtils().passedData} to REPORT-FRAGMENT DATA:- ${arguments?.getString(CommonUtils().type)}")
-        binding.feedbackEditTextLayout.hint = getString(com.sourabhverma.stocksimulator.R.string.feedback_hint, arguments?.getString(CommonUtils().type))
+        binding.feedbackEditTextLayout.hint = getString(R.string.feedback_hint, arguments?.getString(CommonUtils().type))
         binding.feedbackText.text = arguments?.getString(CommonUtils().heading)
     }
 
