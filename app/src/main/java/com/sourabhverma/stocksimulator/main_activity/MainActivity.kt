5db79153ecca21b19cef84933ae38a2d6bcaa0fc
SOURABH KUMAR VERMA <1906286@kiit.ac.in>
2021-10-26 17:45:53 +0530

    added success card

--- app/src/main/java/com/sourabhverma/stocksimulator/main_activity/MainActivity.kt
+++ app/src/main/java/com/sourabhverma/stocksimulator/main_activity/MainActivity.kt
@@ -1,6 +1,13 @@
 package com.sourabhverma.stocksimulator.main_activity
 
 import android.os.Bundle
+import android.text.Spannable
+import android.text.style.ImageSpan
+import android.view.View
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.text.bold
+import androidx.core.text.buildSpannedString
+import com.google.android.material.snackbar.Snackbar
 import com.sourabhverma.stocksimulator.R
 import com.sourabhverma.stocksimulator.base.BaseActivity
 import com.sourabhverma.stocksimulator.databinding.ActivityMainBinding
@@ -24,6 +31,37 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainActivityViewModel>() 
 
     private fun setData() {
         binding.userName.text = SharedPrefManager().getUsername(this)
+        if (SharedPrefManager().getShouldShowFirstSuccessCard(this)) {
+            binding.success.root.visibility = View.VISIBLE
+            binding.success.congratsText.text = buildSpannedString {
+                append(getString(R.string.congrats_text1))
+                bold {
+                    append(" ")
+                    append(getString(R.string.congrats_text2))
+                }
+            }
+            binding.success.checkMostActive.text = buildSpannedString {
+                append(getString(R.string.check_most_active_stock))
+                val d = AppCompatResources.getDrawable(this@MainActivity, R.drawable.ic_right_arrow)
+                val imageSpan = d?.let {
+                    d.setBounds(0, 0, d.intrinsicWidth, d.intrinsicHeight)
+                    ImageSpan(
+                        it,
+                        ImageSpan.ALIGN_BOTTOM)
+                }
+                append("  ")
+                this.setSpan(imageSpan, this.length-1, this.length, Spannable.SPAN_INCLUSIVE_EXCLUSIVE)
+            }
+            binding.success.cancelButton.setOnClickListener {
+                if (SharedPrefManager().setShouldShowFirstSuccessCard(this@MainActivity, false)) {
+                    binding.success.root.visibility = View.GONE
+                } else {
+                    Snackbar.make(binding.root, getString(R.string.something_went_wrong), Snackbar.LENGTH_SHORT).show()
+                }
+            }
+        } else {
+            binding.success.root.visibility = View.GONE
+        }
     }
 
 }
--- app/src/main/java/com/sourabhverma/stocksimulator/utils/SharedPrefManager.kt
+++ app/src/main/java/com/sourabhverma/stocksimulator/utils/SharedPrefManager.kt
@@ -7,12 +7,23 @@ import android.content.SharedPreferences
 class SharedPrefManager {
     private val sharedPrefName = "StockSimulator"
     private val username = "USERNAME"
+    private val shouldShowFirstSuccessCard = "SHOULDSHOWFIRSTSUCCESSCARD"
     private val defaultName = "Investor"
     private lateinit var sharedPreferences : SharedPreferences
 
     fun getUsername(context: Context) : String?{
         sharedPreferences = context.getSharedPreferences(sharedPrefName, MODE_PRIVATE)
         return sharedPreferences.getString(username,defaultName)
+    }
+
+    fun getShouldShowFirstSuccessCard(context: Context) : Boolean{
+        sharedPreferences = context.getSharedPreferences(sharedPrefName, MODE_PRIVATE)
+        return sharedPreferences.getBoolean(shouldShowFirstSuccessCard, true)
+    }
+
+    fun setShouldShowFirstSuccessCard(context: Context, boolean: Boolean) : Boolean{
+        sharedPreferences = context.getSharedPreferences(sharedPrefName, MODE_PRIVATE)
+        return sharedPreferences.edit().putBoolean(shouldShowFirstSuccessCard, boolean).commit()
     }
 
 }
